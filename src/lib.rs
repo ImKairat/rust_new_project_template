942cd3f74240ac797d30a89b7376bd053ae94e79
Kubanychbek uulu Kairat <kubanychbekukairat@gmail.com>
2024-08-15 14:44:48 +0600

    #test; [ADDED] src/lib.rs: - mod tests;         src/main.rs: - The clippy linter;

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// Crate initialize modules here:
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+
+    #[test]
+    fn some_test() {
+        let result = 25;
+        assert_eq!(result, 25);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[allow(clippy::all, clippy::pedantic)]
+
 fn main() {
     println!("Hello, world!");
 }
